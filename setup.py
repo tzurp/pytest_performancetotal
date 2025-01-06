622f82bf0acf1eddda90471febe8dbe70011df69
tzurp <tzur.paldi@outlook.com>
2025-01-06 14:40:48 +0200

    bumped to v0.2.6

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='pytest-performancetotal',
     packages=find_packages(where='src', exclude=['*tests*']),
     package_dir={'': 'src'},
-    version='0.2.5',
+    version='0.2.6',
     author='Tzur Paldi',
     author_email='tzur.paldi@outlook.com',
     maintainer='Tzur Paldi',
