b3c279728df47ab8293347824a5c001ff0b19f19
tzurp <tzur.paldi@outlook.com>
2025-01-06 12:55:54 +0200

    added dropfailed and recentdays options

--- src/pytest_performancetotal/performancetotal.py
+++ src/pytest_performancetotal/performancetotal.py
@@ -3,7 +3,9 @@ import pytest
 from pytest_performancetotal.performance import Performance
 
 def pytest_addoption(parser: pytest.Parser):
-    parser.addoption("--performance-noappend", action='store_true', help='Disables results appending to existing file. Previous data is deleted.')
+    parser.addoption("--performance-noappend", action="store_true", default=False, help="Disables results appending to existing file. Previous data is deleted.")
+    parser.addoption("--performance-drop-failed-results", action="store_true", default=False, help="Drops results for failed tests.")
+    parser.addoption("--performance-recent-days", action="store", default=0, type=float, help="Number of days to consider for performance analysis.")
     
 @pytest.fixture
 def performancetotal(request) -> Generator[Performance, None, None]:
@@ -15,9 +17,11 @@ def performancetotal(request) -> Generator[Performance, None, None]:
 def performance_worker(request: pytest.FixtureRequest) -> Generator[None, None, None]:
     performance = Performance()
     no_append = request.config.getoption('--performance-noappend', False)
+    drop_results_for_failed = request.config.getoption('--performance-drop-failed-results', False)
+    recent_days = request.config.getoption('--performance-recent-days', 0)  
     performance.initialize(request.config.rootdir, no_append)
     yield
-    session_id = request.session.nodeid
-    performance.analyze_results(session_id)
+    # session_id = request.session.nodeid
+    performance.analyze_results(drop_results_for_failed, recent_days)
     
     
